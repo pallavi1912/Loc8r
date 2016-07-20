78d8fa23de0be41877ec86fa387328c04e016472
pallavi191293 <pallavi.verma@innofieed.com>
2016-07-20 14:41:20 +0530

    data added to the view through the controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,57 @@
 /*GET 'home' page*/
 module.exports.homelist = function(req, res) {
-    res.render('location-list', { title: 'Home' });
+    res.render('location-list', {
+        title: 'Loc8r - find a place to work with wifi',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading, Rg 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+        }, {
+            name: 'Cafe Hero',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '200m'
+        }, {
+            name: 'Burger Queen',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 2,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '250m'
+        }]
+    });
 };
 
 /*GET 'Location Info' page*/
 module.exports.locationInfo = function(req, res) {
-    res.render('location-info', { title: 'Location Info' });
+    res.render('location-info', {
+        title: 'Location Info',
+        pageHeader: {
+            title: 'Location Info',
+        },
+        locationInfomation: {
+            name: 'Starcups',
+            rating: 3,
+            address: '125 High Street, Reading, Rg 1PS',
+            openingHours: [{
+                Days: 'Monday-Friday',
+                Duration: '7:00 am - 7:00 pm'
+            }, {
+                Days: 'Saturday',
+                Duration: '8:00 am - 5:00 pm'
+            }, {
+                Days: 'Sunday',
+                Duration: 'Closed'
+            }],
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        }
+    });
 };
 
 /*GET 'Add Review' page*/
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,10 @@
 /*GET 'about' page*/
 module.exports.about = function(req, res) {
-    res.render('generic-text', { title: 'About' });
-};
+        res.render('generic-text', {
+                    title: 'About',
+                    pageHeader: {
+                        title: 'about Us'
+                    },
+                    aboutInfo: 'Loc8r was created to help people find places to sit down and get a bit of work done.'
+                     });
+                };
