88a20d85f8658469e420306a434e595b3021d5c6
souravatwork <sourav.mondal@innofied.com>
2016-08-01 22:32:17 +0530

    changes in location controller

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,11 +1,43 @@
 // var mongoose= require('mongoose');
-var Loc=require('./../models/locations');
+var Loc = require('./../models/locations');
+var ObjectID = require('mongodb').ObjectID;
+
+var ldata = new Loc({
+    name: 'xxxx',
+    address: 'yyyy',
+    rating: 4,
+    facilities: ['sdasdzd'],
+    "coords": [88.423425, 22.591311],
+    "openingTimes": [{ 
+    	"days": "Monday-Friday", 
+    	"opening": "9:00 am", 
+    	"closing": "8:00 pm", 
+    	"closed": false 
+    }, { 
+    	"days": "Saturday", 
+    	"opening": "9:00 am", 
+    	"closing": "5:00 pm", 
+    	"closed": false 
+    }, { 
+    	"days": "Sunday", 
+    	"closed": true 
+    }],
+    reviews: {
+    	author:'ddssxzbdshtdh',
+    	rating:5,
+    	reviewText:'sdgfgkjvkls siug sk skj ssd dsf',
+    	createdOn: new Date()
+    }
+});
+
+// ldata.save(function(){});
+
 // common function for HTTP status code and JSON 
 var sendJsonResponse = function(res, status, content) {
-    res.status(status);
-    res.json(content)
-}
-// placeholder functions
+        res.status(status);
+        res.json(content)
+    }
+    // placeholder functions
 module.exports.locationsCreate = function(req, res) {
     sendJsonResponse(res, 200, { "status": "success" })
 };
@@ -13,49 +45,39 @@ module.exports.locationsCreate = function(req, res) {
 
 //This controller will be used to the location by id
 module.exports.locationsReadOne = function(req, res) {
-	// To check if there is locationid or not
-	if(req.params && req.params.locationid){
-		Loc
-		   .findById(req.params.locationid)
-		   .select('name reviews')
-		   .exec(function(err,location){
-		   	// when location is not found
-		   	if(!location){
-		   		sendJsonResponse(res,404,{
-		   			"message":"location not found"
-		   		})
-		   		return;
-		   	}
-		   	// if error is found
-		   	else if(err){
-		   		sendJsonResponse(res,404,err)
-		   		return;
-		   	}
-		   	var review = {};
-		   	console.log("reviews",location.reviews);
-		   	for(var i=0;i<location.reviews.length;i++) {
-		   		console.log('-------',req.params.reviewid, typeof req.params.reviewid, (location.reviews[i].id), typeof location.reviews[i].id);
-		   		if(req.params.reviewid == (location.reviews[i].id).toString()) {
-		   			review = location.reviews[i];
-		   			break;
-		   		}
-		   	}
-		   	// review=location.reviews.id(ObjectId('req.params.reviewid'))
-		   	console.log("reviews",review,location.reviews,req.params.reviewid);
-		   	sendJsonResponse(res,200,review)
-		   })
+    // To check if there is locationid or not
+    if (req.params && req.params.locationid) {
+        Loc
+            .findById(req.params.locationid)
+            .select('name reviews')
+            .exec(function(err, location) {
+                // when location is not found
+                if (!location) {
+                    sendJsonResponse(res, 404, {
+                        "message": "location not found"
+                    })
+                    return;
+                }
+                // if error is found
+                else if (err) {
+                    sendJsonResponse(res, 404, err)
+                    return;
+                }
+                var review = location.reviews.id(req.params.reviewid);
 
-	}
-	else
-	{
-		sendJsonResponse(res,404,{
-			"message":"locationid not found"
-		});
-	}
-	// Loc
-	//    .findById(req.params.locationid)
-	//    .exec(function(err,location){
-	//    	  sendJsonResponse(res, 200, location)
-	//    })
-  
+                // review=location.reviews.id(ObjectId('req.params.reviewid'))
+                sendJsonResponse(res, 200, review);
+            })
+
+    } else {
+        sendJsonResponse(res, 404, {
+            "message": "locationid not found"
+        });
+    }
+    // Loc
+    //    .findById(req.params.locationid)
+    //    .exec(function(err,location){
+    //    	  sendJsonResponse(res, 200, location)
+    //    })
+
 };
